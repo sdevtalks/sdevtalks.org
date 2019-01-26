532395c59998d3ced45ec5b2d2815393d6baa717
Yuki AKAMATSU <y.akamatsu@ukstudio.jp>
2019-01-26 22:21:34 +0900

    Insert pretty_title and key to each event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,21 @@
+KeyNotGeneratedError = Class.new(StandardError)
+
 class Event < ApplicationRecord
   belongs_to :venue
+
+  def self.create_with_key!(*args)
+    instance = self.new(*args)
+    instance.generate_key
+    instance.save!
+    instance
+  end
+
+  def slug
+    raise KeyNotGeneratedError if self.key.blank?
+    "#{self.pretty_title}-#{self.key}"
+  end
+
+  def generate_key
+    self.key = SecureRandom.alphanumeric(6)
+  end
 end
