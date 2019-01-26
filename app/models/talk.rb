e2a2934b85ba85c408a41f89a3a1085165370da4
SpicyCoffee <7toidrdr.6400@gmail.com>
2019-01-26 23:16:20 +0900

    Fix association between talks and proposals

--- app/models/talk.rb
+++ app/models/talk.rb
@@ -1,5 +1,6 @@
 class Talk < ApplicationRecord
   belongs_to :event
   belongs_to :speaker
-  has_one :proposal, through: :proposals_to_talks
+  has_one :proposals_to_talk
+  has_one :proposal, through: :proposals_to_talk
 end
