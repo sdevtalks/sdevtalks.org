6f3e955aaeee7494231e3d2c5de470b7c958c12f
Yuki AKAMATSU <y.akamatsu@ukstudio.jp>
2019-01-26 22:26:57 +0900

    Implement event page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,4 +4,11 @@ class EventsController < ApplicationController
   def index
     @events = Event.eager_load(:venue).all
   end
+
+  def show
+    pretty_title = params[:slug].split("-")[0..-2].join("-")
+    key          = params[:slug].split("-").last
+
+    @event = Event.find_by!(pretty_title: pretty_title, key: key)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root to: "top#index"
 
   get "/events", to: "events#index"
+  get "/events/:slug", to: "events#show", as: "event"
 end
