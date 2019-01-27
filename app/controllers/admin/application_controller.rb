1ad2ab8c5829881cddc8130ad7e0f8048ad728b3
OSA Shunsuke <hhelibebcnofnenamg@gmail.com>
2019-01-27 09:44:13 +0900

    Enable admin page and basic auth

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -3,7 +3,11 @@ module Admin
     before_action :authenticate_admin
 
     def authenticate_admin
-      # TODO Add authentication logic here.
+      if Rails.env.production?
+        authenticate_or_request_with_http_basic do |username, password|
+          username == 'admin' && password == ENV['BASIC_AUTH_PASSWORD']
+        end
+      end
     end
   end
 end
