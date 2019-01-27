80d0dd3ebc1910dd48a4e1bddd7ef1b5895f5cbc
Yuki AKAMATSU <y.akamatsu@ukstudio.jp>
2019-01-27 09:43:11 +0900

    Add description to all pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def default_meta_tags
+    {
+      site: "s-dev talks",
+      description: "s-dev talksは、サービスづくりに関わる人々が「エンジニア」や「デザイナー」といった職種 にとらわれず、「サービス開発者」として交流するための草の根コミュニティです。"
+    }
+  end
 end
