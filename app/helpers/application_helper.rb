74da675c05e116e7c7c9e2183756c4c152364f6a
Yuki AKAMATSU <y.akamatsu@ukstudio.jp>
2019-01-27 09:49:10 +0900

    Set ogp

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,14 @@ module ApplicationHelper
   def default_meta_tags
     {
       site: "s-dev talks",
-      description: "s-dev talksは、サービスづくりに関わる人々が「エンジニア」や「デザイナー」といった職種 にとらわれず、「サービス開発者」として交流するための草の根コミュニティです。"
+      description: "s-dev talks は、サービスづくりに関わる人々が「エンジニア」や「デザイナー」といった職種 にとらわれず、「サービス開発者」として交流するための草の根コミュニティです。",
+      og: {
+        site_name: "s-dev talks",
+        description: "s-dev talks は、サービスづくりに関わる人々が「エンジニア」や「デザイナー」といった職種 にとらわれず、「サービス開発者」として交流するための草の根コミュニティです。",
+        type: (controller_name == "top" && action_name == "index") ? "site" : "article",
+        url: request.original_url,
+        image: image_url("sdev_ogp@3x.jpg")
+      }
     }
   end
 end
