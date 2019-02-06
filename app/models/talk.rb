beb5a8ae837b6635661f1ed03d43c4e808ac68f6
OSA Shunsuke <hhelibebcnofnenamg@gmail.com>
2019-02-06 23:44:48 +0900

    Set secret_token and build secret_url

--- app/models/talk.rb
+++ app/models/talk.rb
@@ -8,4 +8,16 @@ class Talk < ApplicationRecord
   validates :event_id,      presence: true
   validates :speaker_name,  presence: true
   validates :speaker_email, presence: true
+
+  before_validation :set_secret_token
+
+  def secret_url
+    "https://sdevtalks.org/events/#{event.slug}/talks/#{id}/edit?secret_token=#{secret_token}"
+  end
+
+  private
+
+  def set_secret_token
+    self.secret_token ||= SecureRandom.alphanumeric(32)
+  end
 end
