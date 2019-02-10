53d2534511015780a306c6081a2625cd3e3c5826
OSA Shunsuke <hhelibebcnofnenamg@gmail.com>
2019-02-11 04:10:30 +0900

    Use resources in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   root to: "top#index"
 
-  get "/events", to: "events#index"
-  get "/events/:slug", to: "events#show", as: "event"
+  resources :events, param: :slug, only: %i[index show]
 
   get "/sitemap", to: "sitemaps#index"
 
