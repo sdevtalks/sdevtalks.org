d3023f3d309230f886c1893663aaf10ac8141944
Yuki AKAMATSU <y.akamatsu@ukstudio.jp>
2019-03-26 13:42:45 +0900

    Show published events only

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,13 +3,13 @@
 class EventsController < ApplicationController
   def index
     @page_title = "Events"
-    @events = Event.eager_load(:venue).order(starts_at: :desc)
+    @events = Event.eager_load(:venue).published.order(starts_at: :desc)
   end
 
   def show
     key = params[:slug].split("-").last
 
-    @event = Event.eager_load(:talks, :proposals, :venue).find_by!(key: key)
+    @event = Event.eager_load(:talks, :proposals, :venue).published.find_by!(key: key)
 
     unless params[:slug] == @event.slug
       redirect_to event_path(@event.slug), status: 301
